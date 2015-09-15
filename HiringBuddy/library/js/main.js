67fedfd9d2497ca9e3a640c29057c0c05a9d9a82
Brameshmadhav Srinivasan <sbmadhav@gmail.com>
2015-09-15 18:17:00 +0530

    moving to static json

--- HiringBuddy/library/js/main.js
+++ HiringBuddy/library/js/main.js
@@ -1,5 +1,6 @@
-var candidateName = 'Brameshmadhav S'
-// var timer = require('./library/timer.js');
+var candidateName = 'Brameshmadhav S';
+var fieldsetStr = '';
+
 $('#identity-submit').on('click',function(){
     $('.id-field').html('Welcome ' + candidateName );
     setTimeout(function(){
@@ -11,6 +12,23 @@ $('#identity-submit').on('click',function(){
 
 $('#instructions-submit').on('click',function(){
     $('.instructions').hide();
-    $('.questions').show();
-    timer.start();
+    $.get('questions.json','', function ( dataStr ) {
+        var data = JSON.parse(dataStr);
+        for (var i = 0; i < data.questions.length; i++) {
+            var questionObj = data.questions[i];
+            fieldsetStr += "<fieldset>"
+              + "<legend>Question " + (i + 1) + " :</legend>"
+              + "<div>" + questionObj.question + "</div>"
+              ;
+              for (var j = 0; j < questionObj['answer-choices'].length; j++) {
+                  var answerChoice = questionObj['answer-choices'][j];
+                  fieldsetStr += "<div>" + "<input class= 'answerChoice' type='radio' name='answerChoice' " +  "value='" + j + "'/> " + answerChoice + "</div>";
+              };
+            fieldsetStr += "</fieldset>";
+            ;   
+        };
+        document.getElementById('questions-fieldset').innerHTML = fieldsetStr;
+        $('.questions').show();
+        timer.start();
+    });
 });
